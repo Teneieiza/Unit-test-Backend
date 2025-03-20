4313f95dc83b0598d5af59bab012283c76b335d2
tenmonster <tenmonster@hotmail.com>
2025-03-20 20:14:50 +0700

    Connect Nest to Postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Hotel } from './hotel/hotel.entity';
+import { HotelController } from './hotel/hotel.controller';
+import { HotelService } from './hotel/hotel.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'unitdatatbase',
+      username: 'myuser',
+      password: 'mypassword',
+      autoLoadEntities: true,
+      synchronize: true,
+      retryAttempts: 10,
+      retryDelay: 3000,
+    }),
+    TypeOrmModule.forFeature([Hotel]),
+  ],
+  controllers: [AppController, HotelController],
+  providers: [AppService, HotelService],
 })
 export class AppModule {}
