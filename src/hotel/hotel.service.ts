ae286c98a5c9e4c6bcddf6f9cf3109eeb0fc2a1e
tenmonster <tenmonster@hotmail.com>
2025-03-20 20:36:35 +0700

    Add Method GET by ID

--- src/hotel/hotel.service.ts
+++ src/hotel/hotel.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Hotel } from './hotel.entity';
 import { Repository } from 'typeorm';
@@ -10,7 +10,18 @@ export class HotelService {
     private readonly hotel: Repository<Hotel>,
   ) {}
 
+  //Get all hotel
   async getListHotel(): Promise<Hotel[]> {
     return this.hotel.find();
+  }
+
+  //Get one hotel
+  async getHotelByID(id: number): Promise<Hotel> {
+    const findHotel = await this.hotel.findOne({ where: { id } });
+
+    if (!findHotel)
+      throw new NotFoundException(`Hotel with ID ${id} is not found`);
+
+    return findHotel;
   }
 }
