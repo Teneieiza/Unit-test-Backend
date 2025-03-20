35a446206f91b817d44546a0544b5a76642ac408
tenmonster <tenmonster@hotmail.com>
2025-03-21 00:13:32 +0700

    Add Method Post Search&use format

--- src/hotel/hotel.service.ts
+++ src/hotel/hotel.service.ts
@@ -1,7 +1,14 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Hotel } from './hotel.entity';
 import { Repository } from 'typeorm';
+import { SearchHotelRequest } from './dto/hotel.request.dto';
+import { formatDateTime } from 'src/util/formatdate';
+import { HotelDto } from './dto/hotel.response.dto';
 
 @Injectable()
 export class HotelService {
@@ -17,18 +24,59 @@ export class HotelService {
   }
 
   //GET all hotel
-  async getListHotel(): Promise<Hotel[]> {
-    return this.hotel.find();
+  async getListHotel(): Promise<HotelDto[]> {
+    const allhotel = await this.hotel.find();
+
+    if (!allhotel.length) {
+      throw new NotFoundException('No hotels found');
+    }
+
+    return allhotel.map((hotel) => ({
+      ...hotel,
+      doingtime: formatDateTime(hotel.doingtime),
+    }));
   }
 
   //GET one hotel
-  async getHotelByID(id: number): Promise<Hotel> {
+  async getHotelByID(id: number): Promise<HotelDto> {
     const findHotel = await this.hotel.findOne({ where: { id } });
 
     if (!findHotel)
       throw new NotFoundException(`Hotel with ID ${id} is not found`);
 
-    return findHotel;
+    return {
+      ...findHotel,
+      doingtime: formatDateTime(findHotel.doingtime),
+    };
+  }
+
+  //POST Search hotel
+  async searchHotelByDate(
+    searchhotelrequest: SearchHotelRequest,
+  ): Promise<HotelDto[]> {
+    const { date } = searchhotelrequest;
+
+    const searchDate = new Date(date);
+    if (isNaN(searchDate.getTime())) {
+      throw new BadRequestException('Invalid date format');
+    }
+    const formattedDate = searchDate.toISOString().split('T')[0];
+
+    const hotel = await this.hotel
+      .createQueryBuilder('hotel')
+      .where('Date(hotel.doingtime) = :searchDate', {
+        searchDate: formattedDate,
+      })
+      .getMany();
+
+    if (hotel.length === 0) {
+      throw new NotFoundException(`No hotel on ${formattedDate}`);
+    }
+
+    return hotel.map((hotel) => ({
+      ...hotel,
+      doingtime: formatDateTime(hotel.doingtime),
+    }));
   }
 
   //GET Dashboard hotel
@@ -39,29 +87,34 @@ export class HotelService {
       throw new NotFoundException('No hotels found');
     }
 
-    const allHotel = hotels.length;
+    const formatdate = hotels.map((hotel) => ({
+      ...hotel,
+      doingtime: formatDateTime(hotel.doingtime),
+    }));
 
-    const highestPrice = hotels.reduce((prev, current) =>
+    const allHotel = formatdate.length;
+
+    const highestPrice = formatdate.reduce((prev, current) =>
       current.price > prev.price ? current : prev,
     );
 
-    const lowestPrice = hotels.reduce((prev, current) =>
+    const lowestPrice = formatdate.reduce((prev, current) =>
       current.price < prev.price ? current : prev,
     );
 
-    const lastHotelAdded = hotels.reduce((prev, current) =>
+    const lastHotelAdded = formatdate.reduce((prev, current) =>
       new Date(current.doingtime) > new Date(prev.doingtime) ? current : prev,
     );
 
     return {
-      Data: hotels,
+      Data: formatdate,
       Dashboard: {
         AllHotel: allHotel,
         Price: {
           High: highestPrice.name,
           Low: lowestPrice.name,
         },
-        LastHotelAdd: new Date(lastHotelAdded.doingtime).toISOString(),
+        LastHotelAdd: lastHotelAdded.doingtime,
       },
     };
   }
